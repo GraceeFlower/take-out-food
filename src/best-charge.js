0a16313fb15f761d235e621a2162c8de576db9ae
GraceeFlower <1491644103@qq.com>
2020-01-06 15:32:44 +0800

    [WangMin] feat: use the selected-item array to add the counts for food

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,5 +1,23 @@
 function bestCharge(selectedItems) {
   var itemsList = loadAllItems();
   var promotionsList = loadPromotions();
+  itemsList = addItemsCount(itemsList, selectedItems);
   return /*TODO*/;
+}
+
+function addItemsCount(itemsList, selectedItems) {
+  const regexCount = /(?<=x\s+)\d+/;
+  const regexId = /.*?(?=\s+x)/;
+  let addedCountList = [];
+  selectedItems.forEach((item) => {
+    let itemCount = parseInt(item.match(regexCount));
+    let itemId = item.match(regexId)[0];
+    itemsList.forEach((value) => {
+      if(value.id === itemId) {
+        value["count"] = itemCount;
+        return addedCountList.push(value);
+      }
+    });
+  });
+  return addedCountList;
 }
